8bd80b2d0c3d30c887bc3d094bdf05fa8c84935f
erfan gharib <erfangharib5@gmail.com>
2022-03-24 19:58:15 +0430

    remove database codes

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ module.exports = {
 }
 
 const startApp = async () => {
+    //await require('mongodb').MongoClient.connect(dbUrl, (err, db) => {
+    //    if (err) throw err;
+    //    console.log(`${databaseName} database created!`);
+    //    const dName = db.db(databaseName);
+    //    dName.createCollection('users', (err, res) => {
+    //        if (err) throw err;
+    //        console.log('collection created!');
+    //    });
+    //});
     
     await routes_$arr.forEach((value, index) => {
         app.get(value, (req, res) => {
