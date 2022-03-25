930f7ad3d87450461a6557a20eec64b6bf6c3aae
erfan gharib <erfangharib5@gmail.com>
2022-03-25 14:12:07 +0430

    -

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,6 +1,6 @@
 let url = location.href;
-// const baseUrl = url.slice(0, url.indexOf('com')+3);
-const baseUrl = 'http://localhost:5000';
+const baseUrl = url.slice(0, url.indexOf('com')+3);
+// const baseUrl = 'http://localhost:5000';
 export default baseUrl;
 
 import navbar_$func from '../components/navbar.js';
