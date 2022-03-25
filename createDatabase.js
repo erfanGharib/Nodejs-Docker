23c66bbf8c5b7b43172a7be68e306ec0c7f76c6b
erfan gharib <erfangharib5@gmail.com>
2022-03-25 14:40:50 +0430

    -

--- createDatabase.js
+++ createDatabase.js
@@ -1,3 +1,5 @@
+const databaseName = 'fullstack-practise';
+const dbUrl = 'mongodb://localhost:27017/';
 require('mongodb').MongoClient.connect(dbUrl, (err, db) => {
     if (err) throw err;
     console.log(`${databaseName} database created!`);
