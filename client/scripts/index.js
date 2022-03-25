86e46dcd9048e6a9130ad550deee686e3430a0d3
erfan gharib <erfangharib5@gmail.com>
2022-03-25 13:53:24 +0430

    fix api bugs

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,5 +1,5 @@
 let url = location.href;
-const baseUrl = url.slice(0, url.indexOf('com/')+4);
+const baseUrl = url.slice(0, url.indexOf('com')+4);
 // const baseUrl = 'http://localhost:5000';
 export default baseUrl;
 
